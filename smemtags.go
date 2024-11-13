6fa2042f5c3b09b31c71688d6c7fe1a2f73286b5
Christopher Curtis <ccurtis@gmail.com>
2024-11-13 18:22:41 +0100

    Cleaned up

--- smemtags.go
+++ smemtags.go
@@ -26,7 +26,7 @@ func wraptext(s string) string {
 	for i, c := range s {
 		out += string(c)
 		if i+1 == len(s)/2 {
-			out += "\n"
+			out += "\r\n"
 		}
 	}
 	return out
@@ -35,42 +35,48 @@ func wraptext(s string) string {
 func main() {
 	pdf := fpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
-	pdf.SetMargins(9.0, 13.0, 9.0)
-	pdf.SetFont("Courier", "", 8)
+	//pdf.SetMargins(0.0, 0.0, 0.0)
+	pdf.SetFont("Courier", "", 10)
 
-	rows := 27
+	rows := 25
 	cols := 7
-	qrsize := 8.0
-	colspace := 3.0
+	qrsize := 9.5
+	colspace := 2.95
+	width := 25.4
+	height := 10.0
 	leftmargin := 9.0
 	topmargin := 13.0
+
+	var opt fpdf.ImageOptions
+	opt.ImageType = "png"
+	opt.AllowNegativePosition = false
 
 	for r := 0; r < rows; r++ {
 		for c := 0; c < cols; c++ {
 
-			id := smolid(8)
-			id_split := wraptext(id)
-			fmt.Println(id)
-			qr, err := qrcode.Encode(id, qrcode.Medium, 100)
+			id1 := smolid(3)
+			id2 := smolid(3)
+			id := id1 + id2
+			//id_split := wraptext(id)
+			fmt.Println(id1, id2)
+			qr, err := qrcode.Encode(id, qrcode.Highest, 100)
 			if err != nil {
 				panic("couldn't encode qr")
 			}
 
-			offset_x := leftmargin + float64(c)*(25.98+colspace)
-			offset_y := topmargin + float64(r)*10.0
+			offset_x := leftmargin + float64(c)*(width+colspace)
+			offset_y := topmargin + float64(r)*height
 			text_x := offset_x + 13.0
 			text_y := offset_y
 
 			// put image on pdf
-			var opt fpdf.ImageOptions
-			opt.ImageType = "png"
-			opt.AllowNegativePosition = false
-			_ = pdf.RegisterImageOptionsReader(id, opt, bytes.NewReader(qr))
-			pdf.ImageOptions(id, offset_x, offset_y, qrsize, qrsize, false, opt, 0, "")
-
 			fmt.Printf("Placing (%f, %f), (%f, %f)\n", offset_x, offset_y, text_x, text_y)
-			pdf.SetXY(offset_x, offset_y)
-			pdf.CellFormat(25.4, 10.0, id_split, "1", 0, "LM", false, 0, "")
+			_ = pdf.RegisterImageOptionsReader(id, opt, bytes.NewReader(qr))
+			pdf.SetXY(offset_x+qrsize, offset_y)
+			pdf.CellFormat(width-qrsize, height/2, id1, "0", 0, "CB", false, 0, "")
+			pdf.SetXY(offset_x+qrsize, offset_y+5.0)
+			pdf.CellFormat(width-qrsize, height/2, id2, "0", 0, "CT", false, 0, "")
+			pdf.ImageOptions(id, offset_x, offset_y, qrsize, qrsize, false, opt, 0, "")
 		}
 	}
 	e := pdf.Error()
